e5be40ff806bc7addd7c6328cc95244c57e895b7
k1z3 <koizumi.takahiro.qg@alumni.tsukuba.ac.jp>
2023-09-12 12:15:53 +0900

    [update] チェックボックスのイベント名を修正

--- frontend/src/pages/RegisterAndEditMangapage.tsx
+++ frontend/src/pages/RegisterAndEditMangapage.tsx
@@ -14,105 +14,109 @@ import NumerInputRow from '../components/NumberInputRow';
 import NumberInputRow from '../components/NumberInputRow';
 import { useNavigate } from 'react-router-dom';
 
-const RegisterAndEditMangaPage: React.FC<{isEditMode: boolean, data: PostMangaData}> = ({
-  isEditMode, data
-}) => {
+const RegisterAndEditMangaPage: React.FC<{
+  isEditMode: boolean;
+  data: PostMangaData;
+}> = ({ isEditMode, data }) => {
   const navigation = useNavigate();
   const [mangaData, setMangaData] = React.useState(defaultPostMangaData);
   const [isSerial, setIsSerial] = React.useState(false);
   useEffect(() => {
     if (data) {
-      console.log("defaultValue")
-      console.log(data)
+      console.log('defaultValue');
+      console.log(data);
       setMangaData(data);
     }
-  }, [])
+  }, []);
 
   return (
-    <Container sx={{py: 4}}>
+    <Container sx={{ py: 4 }}>
       <Typography variant="h6" gutterBottom>
-        {isEditMode ? "漫画の編集画面" : "漫画の登録画面"}
+        {isEditMode ? '漫画の編集画面' : '漫画の登録画面'}
       </Typography>
       <Container maxWidth="lg">
         <Grid container spacing={4}>
           <TextInputRow
             isHalf={false}
             required={true}
-            name='mangaTitle'
-            labelText='漫画のタイトル'
-            placeholderText='漫画のタイトル'
+            name="mangaTitle"
+            labelText="漫画のタイトル"
+            placeholderText="漫画のタイトル"
             value={mangaData.title}
             isTextField={true}
-            onValueChange={(value) => mangaData.title = value}
+            onValueChange={(value) => (mangaData.title = value)}
           />
-          
+
           <TextInputRow
             isHalf={false}
             required={true}
             name="mangaAuthor"
-            labelText='漫画の作者'
-            placeholderText='漫画の作者'
+            labelText="漫画の作者"
+            placeholderText="漫画の作者"
             value={mangaData.author}
             isTextField={true}
-            onValueChange={(value) => mangaData.author = value}
+            onValueChange={(value) => (mangaData.author = value)}
           />
 
           <TextInputRow
             isHalf={false}
             required={false}
             name="mangaSummary"
-            labelText='漫画の概要'
-            placeholderText='漫画の概要'
+            labelText="漫画の概要"
+            placeholderText="漫画の概要"
             value={mangaData.summary}
             isTextField={false}
-            onValueChange={(value) => mangaData.summary = value}
+            onValueChange={(value) => (mangaData.summary = value)}
           />
 
           <TextInputRow
             isHalf={false}
             required={true}
             name="mangaGenre"
-            labelText='漫画のジャンル'
-            placeholderText='漫画のジャンル'
+            labelText="漫画のジャンル"
+            placeholderText="漫画のジャンル"
             value={mangaData.genre}
             isTextField={true}
-            onValueChange={(value) => mangaData.genre = value}
+            onValueChange={(value) => (mangaData.genre = value)}
           />
 
           <TextInputRow
             isHalf={false}
             required={false}
             name="mangaEditor"
-            labelText='漫画の編集者'
-            placeholderText='漫画の編集者'
+            labelText="漫画の編集者"
+            placeholderText="漫画の編集者"
             value={mangaData.editor}
             isTextField={true}
-            onValueChange={(value) => mangaData.editor = value}
+            onValueChange={(value) => (mangaData.editor = value)}
           />
 
           <TextInputRow
             isHalf={false}
             required={false}
             name="mangaPictureUrl"
-            labelText='漫画の画像'
-            placeholderText='漫画の画像'
+            labelText="漫画の画像"
+            placeholderText="漫画の画像"
             value={mangaData.picture_url}
             isTextField={true}
-            onValueChange={(value) => mangaData.picture_url = value}
+            onValueChange={(value) => (mangaData.picture_url = value)}
           />
 
           <TextInputRow
             isHalf={false}
             required={false}
             name="mangaOthers"
-            labelText='その他'
-            placeholderText='その他'
+            labelText="その他"
+            placeholderText="その他"
             value={mangaData.others}
             isTextField={true}
-            onValueChange={(value) => mangaData.others = value}
+            onValueChange={(value) => (mangaData.others = value)}
           />
 
-          <Grid item xs={6} sm={6}
+          <Grid
+            item
+            xs={6}
+            sm={6}
             sx={{
               display: 'flex',
               alignItems: 'center',
@@ -121,18 +125,17 @@ const RegisterAndEditMangaPage: React.FC<{isEditMode: boolean, data: PostMangaDa
           >
             <Checkbox
               checked={isSerial}
-              onChange={(e) => {
-                // setStateは非同期で動くので、ここで反映されていないみたいなので新しい値を変数に入れておく
-                // https://zenn.dev/takuty/articles/c032310a6643ac
-                const newVal = !isSerial
-                setIsSerial(newVal)
-                mangaData.is_serialization = newVal
-                console.log(mangaData.is_serialization)
-            }}/>
-            <Typography sx={{
-              marginLeft: '8px',
-              width: '200px',
-            }}>
+              onClick={(e) => {
+                setIsSerial(!isSerial);
+                mangaData.is_serialization = isSerial;
+              }}
+            />
+            <Typography
+              sx={{
+                marginLeft: '8px',
+                width: '200px',
+              }}
+            >
               完結済みか
             </Typography>
           </Grid>
@@ -141,26 +144,29 @@ const RegisterAndEditMangaPage: React.FC<{isEditMode: boolean, data: PostMangaDa
             isHalf={true}
             required={false}
             name="mangaEdition"
-            labelText='版数'
-            placeholderText='版数'
+            labelText="版数"
+            placeholderText="版数"
             value={mangaData.edition}
             isTextField={true}
-            onValueChange={(value) => mangaData.edition = Number(value)}
+            onValueChange={(value) => (mangaData.edition = Number(value))}
           />
 
-          <DatePicker value={mangaData.volumeone_at} onValueChange={(value) => {
-            mangaData.volumeone_at = value;
-          }}/>
+          <DatePicker
+            value={mangaData.volumeone_at}
+            onValueChange={(value) => {
+              mangaData.volumeone_at = value;
+            }}
+          />
 
           <NumberInputRow
             isHalf={true}
             required={true}
             name="mangaEdition"
-            labelText='総巻数'
-            placeholderText='総巻数'
+            labelText="総巻数"
+            placeholderText="総巻数"
             value={mangaData.volumes}
             isTextField={true}
-            onValueChange={(value) => mangaData.volumes = value}
+            onValueChange={(value) => (mangaData.volumes = value)}
           />
         </Grid>
       </Container>
@@ -172,19 +178,31 @@ const RegisterAndEditMangaPage: React.FC<{isEditMode: boolean, data: PostMangaDa
           alignItems: 'center',
         }}
       >
-        {
-          isEditMode ?
-          <Button onClick={() => {
-            // 更新する処理
-            console.log(mangaData)
-            // navigation("/login/publisher")
-          }} variant="contained" sx={{ marginTop: '20px' }}>更新`</Button> :
-          <Button onClick={() => {
-            // 登録する処理
-            console.log(mangaData)
-            // navigation("/login/publisher")
-          }} variant="contained" sx={{ marginTop: '20px' }}>登録</Button>
-        }
+        {isEditMode ? (
+          <Button
+            onClick={() => {
+              // 更新する処理
+              console.log(mangaData);
+              // navigation("/login/publisher")
+            }}
+            variant="contained"
+            sx={{ marginTop: '20px' }}
+          >
+            更新`
+          </Button>
+        ) : (
+          <Button
+            onClick={() => {
+              // 登録する処理
+              console.log(mangaData);
+              // navigation("/login/publisher")
+            }}
+            variant="contained"
+            sx={{ marginTop: '20px' }}
+          >
+            登録
+          </Button>
+        )}
       </Container>
     </Container>
   );
