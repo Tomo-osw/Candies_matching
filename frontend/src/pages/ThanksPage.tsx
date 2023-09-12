eabc1c2596744452910a98a1f0944bab186e632b
SEKI-YUA <zard.since1991@gmail.com>
2023-09-12 17:28:29 +0900

    ありがとうページにホーム画面に戻るボタンの追加とヘッダーの追加

--- frontend/src/pages/ThanksPage.tsx
+++ frontend/src/pages/ThanksPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MangaItem from '../components/MangaItem';
 import {fakeMultipleMangaData } from '../data/MangaFakeData';
 import {
@@ -7,35 +7,42 @@ import {
   Grid,
   Typography,
 } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { HeaderBar } from '../components/Header';
 
 const ThanksPage: React.FC = () => {
   return (
-    <Container
-        sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-            height: '100vh',
-        }}
-    >
-        <Typography
-        variant='h1'
+    <Box sx={{width: "100%"}}>
+        <HeaderBar/>
+        <Container
             sx={{
-                fontSize: {
-                    xs: '1.2rem',
-                    md: '1.5rem',
-                    lg: '1.8rem',
-                }
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                height: '90vh',
             }}
         >
-            ありがとうございます。
-        </Typography>
-        <Box sx={{ height: '20px' }} />
-        <Typography>担当者から折り返し連絡をするのでお待ちください。</Typography>
-    </Container>
+            <Typography
+            variant='h1'
+                sx={{
+                    fontSize: {
+                        xs: '1.2rem',
+                        md: '1.5rem',
+                        lg: '1.8rem',
+                    }
+                }}
+            >
+                ありがとうございます。
+            </Typography>
+            <Box sx={{ height: '20px' }} />
+            <Typography>担当者から折り返し連絡をするのでお待ちください。</Typography>
+            <Link to="/home">
+                <Typography>ホーム画面に戻る</Typography>
+            </Link>
+        </Container>
+    </Box>
   );
 };
 
