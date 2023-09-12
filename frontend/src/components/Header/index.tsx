98639f4e253c27ea8883a62ea9f31c0f53f9c94d
naru-77x <naru.fumi@icloud.com>
2023-09-12 17:18:47 +0900

    headerにリンクを埋め込み

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export const HeaderBar = () => {
   return (
     <AppBar position="sticky" style={{ marginBottom: '1em' }}>
       <Toolbar variant="dense">
-        <Typography variant="h6" color="inherit" component="div">
-          Candies
-        </Typography>
+        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+          <Typography variant="h6" color="inherit" component="div">
+            Candies
+          </Typography>
+        </Link>
       </Toolbar>
     </AppBar>
   );
