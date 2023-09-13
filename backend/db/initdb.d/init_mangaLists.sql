98cf0b5b8188e7272b550ce7a140056af13b6f34
Tomo-osw <84332899+Tomo-osw@users.noreply.github.com>
2023-09-13 10:31:11 +0900

    Update init_mangaLists.sql

--- backend/db/initdb.d/init_mangaLists.sql
+++ backend/db/initdb.d/init_mangaLists.sql
@@ -43,77 +43,77 @@ INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volume
 VALUES (3, '推しの子', '赤坂アカ・横槍メンゴ', '主人公が推していたアイドルの子供に生まれ変わる「転生系」でサスペンス要素や芸能界の闇へ切り込むリアルさが特徴', '2020-04-23', 12, 1, true, '転生×恋愛×サスペンス', '編集者K', 'https://dosbg3xlm0x1t.cloudfront.net/images/items/9784088916507/1200/9784088916507.jpg', '主題歌：アイドル（YOASOBI）');
 
 -- 漫画12: 東京卍リベンジャーズ
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (4, '東京卍リベンジャーズ', '和久井健', '過去を改変するタイムリープ', '2017-03-01', 25, 1, true, 'アクション', '編集者L', 'https://cv.bkmkn.kodansha.co.jp/9784063959383/9784063959383_w.jpg', 'その他の情報12');
 
 -- 漫画13: マギ
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (7, 'マギ', '大高忍', '魔法と冒険のファンタジー', '2009-06-03', 37, 1, true, 'ファンタジー', '編集者MM', 'https://www.shogakukan.co.jp/thumbnail/books/09122063', 'その他の情報39');
 
 -- 漫画14: デスノート
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, 'デスノート', '大場つぐみ', '死神のノートの戦い', '2003-12-02', 12, 1, false, 'サスペンス', '編集者N', 'https://dosbg3xlm0x1t.cloudfront.net/images/items/9784086194860/1200/9784086194860.jpg', 'その他の情報14');
 
 -- 漫画15: ハンターハンター
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, 'ハンターハンター', '富坚秀俊', 'ハンター試験を受ける冒険者たち', '1998-03-03', 36, 1, true, 'アクション', '編集者K', 'https://www.shonenjump.com/j/rensai/img/main_hunter.jpg', 'その他の情報11');
 
 -- 漫画16: ジョジョの奇妙な冒険
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, 'ジョジョの奇妙な冒険', '荒木飛呂彦', '異能バトルの系譜', '1987-11-16', 132, 1, true, 'アクション', '編集者P', 'https://www.kinokuniya.co.jp/images/goods/ar2/web/imgdata2/40885/4088511263.jpg', 'その他の情報16');
 
 -- 漫画17: 終末のハーレム
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, '終末のハーレム', '水無月すう', '男性が極少数の女性と生き抜く世界', '2016-04-08', 13, 1, true, 'サスペンス', '編集者Q', 'https://m.media-amazon.com/images/I/71V5kbvEzEL._AC_UF1000,1000_QL80_.jpg', 'その他の情報17');
 
 -- 漫画18: プロメア
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (8, 'プロメア', 'TRIGGER', '炎と戦う超能力者', '2019-05-24', 1, 1, false, 'SF', '編集者R', 'https://promare-movie.com/wp-content/themes/promare-theme_v1/asset/img/top/kv-teaser-3.jpg', 'その他の情報18');
 
 -- 漫画19: 僕のヒーローアカデミア
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, '僕のヒーローアカデミア', '堀越耕平', '個性を持つヒーローたちの成長', '2014-07-07', 35, 1, true, 'アクション', '編集者S', 'https://www.shonenjump.com/j/rensai/img/main_myhero.jpg', 'その他の情報19');
 
 -- 漫画20: ダイの大冒険
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, 'ダイの大冒険', '三条陸', '勇者ダイの冒険と戦い', '1989-10-03', 37, 1, true, 'ファンタジー', '編集者T', 'https://m.media-amazon.com/images/I/51pQJR2QUbL._SY445_SX342_.jpg', 'その他の情報20');
 
 -- 漫画21: アルスラーン戦記
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (4, 'アルスラーン戦記', '荒川弘', '若き王アルスラーンの冒険', '2013-08-09', 14, 1, true, 'ファンタジー', '編集者U', 'https://m.media-amazon.com/images/I/51uLm410LSL._SY445_SX342_.jpg', 'その他の情報21');
 
 -- 漫画22: グラップラー刃牙
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (9, 'グラップラー刃牙', '板垣恵介', '闘技場での格闘戦', '1991-05-08', 37, 1, true, 'アクション', '編集者V', 'https://m.media-amazon.com/images/I/51cHa8DJCVL._SY445_SX342_.jpg', 'その他の情報22');
 
 -- 漫画23: デジモンアドベンチャー LAST EVOLUTION
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, 'デジモンアドベンチャー LAST EVOLUTION', '藤本弘', 'デジモンバーチャルワールド冒険', '2020-02-07', 1, 1, true, 'アクション', '編集者W', 'https://m.media-amazon.com/images/I/51zTlMwMmlL._SY445_SX342_.jpg', 'その他の情報23');
 
 -- 漫画24: メイドインアビス
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (10, 'メイドインアビス', 'つくしあきひと', '未知の深淵への冒険', '2012-01-20', 9, 1, true, 'ファンタジー', '編集者X', 'https://m.media-amazon.com/images/I/81JPXapC4lL._SL1245_.jpg', 'その他の情報24');
 
 -- 漫画25: おしん
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (11, 'おしん', '福田麻琴', '昭和時代の女性の生涯', '1974-06-03', 7, 1, true, 'ドラマ', '編集者Y', 'https://m.media-amazon.com/images/I/51AKXsKzoaL._SY445_SX342_.jpg', 'その他の情報25');
 
 -- 漫画26: トリコ
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, 'トリコ', '藤本栄一', '美食狩りの冒険', '2008-05-26', 43, 1, true, 'アクション', '編集者Z', 'https://www.shonenjump.com/j/rensai/img/main_toriko.jpg', 'その他の情報26');
 
 -- 漫画27: GTO
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (4, 'GTO', '藤本栄一', '変わり者教師の学園生活', '2000-09-22', 15, 1, true, 'ドラマ', '編集者AA', 'https://cv.bkmkn.kodansha.co.jp/9784063124118/9784063124118_w.jpg', 'その他の情報27');
 
 -- 漫画28: ゴルゴ13
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (7, 'ゴルゴ13', 'さいとう・たかを', '冷徹な暗殺者の活躍', '1968-10-25', 200, 1, true, 'アクション', '編集者LL', 'https://m.media-amazon.com/images/I/51kZ-Zw-q0L.jpg', 'その他の情報38');
 
 -- 漫画29: クレイモア
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (3, 'クレイモア', '夢枕獏', '魔物を狩る戦士たち', '2001-05-22', 27, 1, true, 'ダークファンタジー', '編集者CC', 'https://m.media-amazon.com/images/I/51U2ELMNZaL._SY445_SX342_.jpg', 'その他の情報29');
 
 -- 漫画30: シュタインズ・ゲート
-INSERT INTO manga (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
+INSERT INTO mangaLists (company_id, title, author, summary, volumeone_at, volumes, edition, is_serialization, genre, editor, picture_url, others)
 VALUES (2, 'シュタインズ・ゲート', '5pb. & Nitroplus', 'タイムトラベルの物語', '2010-09-03', 3, 1, true, 'SF', '編集者DD', 'https://m.media-amazon.com/images/I/51Sv2hQp5yL._SY445_SX342_.jpg', 'その他の情報30');
